c57437b877365dbea4dea77847f9e9383165e117
Carlos Cuesta <carloscuesta@me.com>
2019-11-24 13:47:17 +0100

    refactor(components): Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,82 @@
+// @flow
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import { rhythm, scale } from '../../utils/typography'
+
+type Props = {
+  location: { pathname: string },
+  title: string,
+  children: React.Node
+}
+
+const Layout = (props: Props) => {
+  const { location, title, children } = props
+  // $FlowFixMe
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
